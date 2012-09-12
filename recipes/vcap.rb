312b83d854a9d7b4fe77a67ab4dacafd367bf5e0
Samir ASNOUN <samir.asnoun@gmail.com>
2012-09-12 16:16:17 +0200

    modify the source used by recipe to download vcap repo

--- recipes/vcap.rb
+++ recipes/vcap.rb
@@ -1,13 +1,23 @@
+%w{unzip}.each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+
+
 bash "make vcap local copy from local http repo" do
   user "root" 
   cwd "/srv" 
   code <<-EOH
-  apt-get install unzip
-  wget http://10.1.1.241/master
-  unzip master
-  mv ./cloudfoundry-vcap-* ./vcap
+  wget http://10.1.1.241/master -O vcap_zip
+  unzip vcap_zip
+  mkdir vcap
+  mv ./`ls | grep cloudfoundry-vcap*`/* ./vcap
+  rm -rf ./`ls | grep cloudfoundry-vcap*`
+  rm vcap_zip
   EOH
- not_if {"ls /srv/vcap/dev_setup" }
+# not_if {"ls /srv/vcap/dev_setup" }
 
 end
 
